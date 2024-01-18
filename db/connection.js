4c6e89f99f7ed1c4e0ddde11efac4c7ff0ab9910
Mohammed Sajjadul <mohammedsajjadul@gmail.com>
2024-01-18 12:02:16 +0000

    added corresponding files to host application

--- db/connection.js
+++ db/connection.js
@@ -4,9 +4,14 @@ const ENV = process.env.NODE_ENV || 'development';
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
 });
-
-if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+const config= {}
+if(ENV === 'production'){
+  config.connectionString = process.env.DATABASE_URL;
+  config.max = 2;
 }
 
-module.exports = new Pool();
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE not set or DATABASE_URL not set');
+}
+
+module.exports = new Pool(config);
